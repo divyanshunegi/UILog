78e2987a203db8b8c9a39a76a28f77ea9156bdef
Divyanshu Negi <divyanshunegi@Divyanshus-MacBook-Pro.local>
2017-06-16 01:05:04 +0530

    added the colors and formating to the log messages

--- uiloglib/src/main/java/divyanshu/uiloglib/ListAdapter.java
+++ uiloglib/src/main/java/divyanshu/uiloglib/ListAdapter.java
@@ -1,12 +1,15 @@
 package divyanshu.uiloglib;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class ListAdapter extends ArrayAdapter<LogMessage> {
@@ -32,11 +35,47 @@ public class ListAdapter extends ArrayAdapter<LogMessage> {
             TextView tt1 = (TextView) v.findViewById(R.id.logMessage);
 
             if (tt1 != null) {
-                tt1.setText(p.getLogMessage());
+
+                tt1.setText(getFormatedDateTime(Long.parseLong(p.getLogTimestamp()))
+                        +"/"+p.getLogType().toString().toLowerCase().charAt(0)+":"
+                        +"    "
+                        +p.getLogTag()
+                        +"    "
+                        +p.getLogMessage()
+                );
+
+                switch (p.getLogType()){
+                    case VERBOSE:
+                        tt1.setTextColor(ContextCompat.getColor(getContext(),R.color.verbose));
+                        break;
+                    case INFO:
+                        tt1.setTextColor(ContextCompat.getColor(getContext(),R.color.info));
+                        break;
+                    case WARNING:
+                        tt1.setTextColor(ContextCompat.getColor(getContext(),R.color.warning));
+                        break;
+                    case ERROR:
+                        tt1.setTextColor(ContextCompat.getColor(getContext(),R.color.error));
+                        break;
+                    case DEBUG:
+                        tt1.setTextColor(ContextCompat.getColor(getContext(),R.color.debug));
+                        break;
+                }
+
             }
         }
 
         return v;
+    }
+
+
+    private String getFormatedDateTime(long timeinEpoch){
+        if (timeinEpoch == 0) {
+            return "";
+        }
+        Date date = new Date(timeinEpoch);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss a");
+        return formatter.format(date);
     }
 
 }
